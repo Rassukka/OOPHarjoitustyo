f4d09b323d092deae31b7f3e3cbd79be594ccc84
BuildTools <unconfigured@null.spigotmc.org>
2017-02-11 20:20:55 +0200

    Virheviesti

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Miten tämä toimia?");
+        System.err.println("Tämä Laite ei toimi");
     }
 }
