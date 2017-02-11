cfd00877826392f52bc9705bb31722c34cd2e596
BuildTools <unconfigured@null.spigotmc.org>
2017-02-11 20:27:03 +0200

    Virheviestti

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Miten tämä toimia?");
-        System.err.println("Tämä Laite ei toimi");
+        System.err.println("Tämä Laite ei toimmi");
     }
 }
