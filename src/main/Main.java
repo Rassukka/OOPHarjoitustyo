e47133b579abe86445f5a99ed7f64978461ede2c
Rasmus <rasmus.haavisto@gmail.com>
2017-02-12 14:55:55 +0200

    Perus database ja testidata toimii!

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,26 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Miten tämä toimia?");
-        System.err.println("Tämä Laite ei toimmi");
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM kokeilu");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
     }
 }
