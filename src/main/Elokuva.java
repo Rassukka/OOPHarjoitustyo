48e986ec8afc926464d4bacf8873efdbb84bbbf6
duralmina <duralmina@gmail.com>
2017-02-23 20:03:36 +0200

    elokuva ja teatteri luokkia alkuun

---
+++ src/main/Elokuva.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * Created by Durina on 23.2.2017.
+ */
+public class Elokuva {
+
+    private Tyyppi tyyppi;
+    private boolean is2D;
+    private String paaHenkilo;
+    private int ikaraja;
+
+    public Elokuva() {
+    }
+}
---
+++ src/main/Elokuvateatteri.java
@@ -0,0 +1,22 @@
+package main;
+
+/**
+ * Created by Durina on 23.2.2017.
+ */
+public class Elokuvateatteri {
+
+    private final String NIMI = "RasAndVoi Teatteri";
+    private ArrayList<Elokuva> elokuvat;
+
+    public Elokuvateatteri(){
+
+    }
+
+}
+
+class Sali extends Elokuvateatteri{
+
+    private int rivit;
+    private int paikatRiveilla;
+
+}
